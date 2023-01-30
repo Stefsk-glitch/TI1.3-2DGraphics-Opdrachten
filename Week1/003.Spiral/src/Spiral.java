ba2bcea791e727dc6a87321614e4ac7de92d44ad
Stef Kraaijeveld <itiredplease@gmail.com>
2023-01-30 19:33:40 +0100

    added spiral

--- Week1/003.Spiral/src/Spiral.java
+++ Week1/003.Spiral/src/Spiral.java
@@ -9,9 +9,12 @@ import javafx.stage.Stage;
 import org.jfree.fx.FXGraphics2D;
 
 public class Spiral extends Application {
+    private Canvas canvas;
+
     @Override
-    public void start(Stage primaryStage) throws Exception {
-        Canvas canvas = new Canvas(1920, 1080);
+    public void start(Stage primaryStage) throws Exception
+    {
+        canvas = new Canvas(400, 400);
         draw(new FXGraphics2D(canvas.getGraphicsContext2D()));
         primaryStage.setScene(new Scene(new Group(canvas)));
         primaryStage.setTitle("Spiral");
@@ -19,12 +22,40 @@ public class Spiral extends Application {
     }
     
     
-    public void draw(FXGraphics2D graphics) {
+    public void draw(FXGraphics2D graphics)
+    {
+        graphics.setColor(Color.red);
+        graphics.drawLine(0,(int)this.canvas.getHeight()/2,1000,(int)this.canvas.getHeight()/2);
+
+        graphics.setColor(Color.green);
+        graphics.drawLine((int)this.canvas.getWidth()/2,0,(int)this.canvas.getWidth()/2,1000);
+
+        graphics.translate(this.canvas.getWidth()/2, this.canvas.getHeight()/2);
+
+        graphics.setColor(Color.black);
+
+        int nPoints = 720;
+
+        int x1 = 0;
+        int y1 = 0;
+        int x2;
+        int y2;
+
+        for (int i = 0; i < nPoints; i++)
+        {
+            double t = i * Math.PI / 180;
+            x2 = (int) ((int) 20*t*Math.cos(t));
+            y2 = (int) ((int) 20*t*Math.sin(t));
+            graphics.drawLine(x1, y1, x2, y2);
+            x1 = x2;
+            y1 = y2;
+        }
     }
     
     
     
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         launch(Spiral.class);
     }
 
