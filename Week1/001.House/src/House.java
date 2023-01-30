2f19879036afe99bfa4321818fe26e3850ba7c70
Stef <s.kraaijeveld3@student.avans.nl>
2023-01-30 15:02:52 +0100

    added house

--- Week1/001.House/src/House.java
+++ Week1/001.House/src/House.java
@@ -5,10 +5,12 @@ import javafx.scene.canvas.Canvas;
 import javafx.stage.Stage;
 import org.jfree.fx.FXGraphics2D;
 
-public class House extends Application {
+public class House extends Application
+{
     @Override
-    public void start(Stage primaryStage) throws Exception {
-        Canvas canvas = new Canvas(1920, 1080);
+    public void start(Stage primaryStage) throws Exception
+    {
+        Canvas canvas = new Canvas(640, 480);
         draw(new FXGraphics2D(canvas.getGraphicsContext2D()));
         primaryStage.setScene(new Scene(new Group(canvas)));
         primaryStage.setTitle("House");
@@ -16,12 +18,32 @@ public class House extends Application {
     }
 
 
-    public void draw(FXGraphics2D graphics) {
+    public void draw(FXGraphics2D graphics)
+    {
+        // house
+        graphics.drawLine(400,400,100,400);
+        graphics.drawLine(400,400,400,200);
+        graphics.drawLine(100, 400, 100, 200);
+        graphics.drawLine(100, 200, 250, 50);
+        graphics.drawLine(400, 200, 250, 50);
+
+        // door
+        graphics.drawLine(150, 400, 150, 300);
+        graphics.drawLine(150, 300, 200, 300);
+        graphics.drawLine(200, 300, 200, 400);
+
+        // window
+        graphics.drawLine(250, 275, 375,275);
+        graphics.drawLine(250, 350, 375,350);
+        graphics.drawLine(375, 350, 375,275);
+        graphics.drawLine(250, 275, 250,350);
+
     }
 
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         launch(House.class);
     }
 
