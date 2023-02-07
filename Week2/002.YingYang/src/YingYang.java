e1f7b7218e015abbe91b6c9a52d578a1953d8487
Stef <s.kraaijeveld3@student.avans.nl>
2023-02-07 14:23:35 +0100

    no message

--- Week2/002.YingYang/src/YingYang.java
+++ Week2/002.YingYang/src/YingYang.java
@@ -33,6 +33,39 @@ public class YingYang extends Application {
         graphics.setTransform(new AffineTransform());
         graphics.setBackground(Color.white);
         graphics.clearRect(0, 0, (int) canvas.getWidth(), (int) canvas.getHeight());
+        graphics.translate(this.canvas.getWidth() / 2, this.canvas.getHeight() / 2);
+
+        Area yinyangCircle = new Area(new Ellipse2D.Double(-100, -100, 200, 200));
+
+        Area yangCircle = new Area(new Ellipse2D.Double(-50, 0, 100, 100));
+        Area yangSmallCircle = new Area(new Ellipse2D.Double(-12.5, -50 - 12.5, 25, 25));
+
+        Area yinCircle = new Area(new Ellipse2D.Double(-50, -100, 100, 100));
+        Area yinSmallCircle = new Area(new Ellipse2D.Double(-12.5, 25 + 12.5, 25, 25));
+
+        yangCircle.add(yangSmallCircle);
+        yangCircle.subtract(yinSmallCircle);
+        yinCircle.add(yinSmallCircle);
+        yinCircle.subtract(yangSmallCircle);
+
+        Area yangSideRectangle = new Area(new Rectangle2D.Double(-100, -100, 100, 200));
+        Area yinSideRectangle = new Area(new Rectangle2D.Double(0, -100, 100, 200));
+
+        Area yinSide = new Area(yinyangCircle);
+        yinSide.subtract(yangSideRectangle);
+        yinSide.subtract(yangCircle);
+        yinSide.add(yinCircle);
+
+        Area yangSide = new Area(yinyangCircle);
+        yangSide.subtract(yinSideRectangle);
+        yangSide.subtract(yinCircle);
+        yangSide.add(yangCircle);
+
+        graphics.setColor(Color.WHITE);
+        graphics.fill(yangSide);
+        graphics.setColor(Color.BLACK);
+        graphics.fill(yinSide);
+        graphics.draw(yinyangCircle);
     }
 
 
