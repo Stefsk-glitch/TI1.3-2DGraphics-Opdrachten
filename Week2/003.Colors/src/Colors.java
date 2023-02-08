af03cc25cd51b1b20866e100ca17c3595a664355
Stef Kraaijeveld <itiredplease@gmail.com>
2023-02-08 14:31:52 +0100

    Update Colors.java

--- Week2/003.Colors/src/Colors.java
+++ Week2/003.Colors/src/Colors.java
@@ -1,5 +1,6 @@
 import java.awt.*;
 import java.awt.geom.*;
+import java.util.ArrayList;
 
 import javafx.application.Application;
 
@@ -33,6 +34,35 @@ public class Colors extends Application {
         graphics.setTransform(new AffineTransform());
         graphics.setBackground(Color.white);
         graphics.clearRect(0, 0, (int) canvas.getWidth(), (int) canvas.getHeight());
+
+        ArrayList<Color> colors = new ArrayList<>();
+
+        colors.add(Color.BLACK);
+        colors.add(Color.BLUE);
+        colors.add(Color.CYAN);
+        colors.add(Color.DARK_GRAY);
+        colors.add(Color.GRAY);
+        colors.add(Color.GREEN);
+        colors.add(Color.LIGHT_GRAY);
+        colors.add(Color.MAGENTA);
+        colors.add(Color.ORANGE);
+        colors.add(Color.PINK);
+        colors.add(Color.RED);
+        colors.add(Color.WHITE);
+        colors.add(Color.YELLOW);
+
+        double width = canvas.getWidth() / colors.size();
+
+        double unit = width / 6;
+        for (int i = 0; i < colors.size(); i++) {
+            Color old = graphics.getColor();
+            graphics.setColor(colors.get(i));
+            Rectangle2D.Double rectangle = new Rectangle2D.Double(width * i + unit, unit, unit * 4, unit * 4);
+            graphics.fill(rectangle);
+            graphics.setColor(old);
+            graphics.draw(rectangle);
+        }
+
     }
 
 
