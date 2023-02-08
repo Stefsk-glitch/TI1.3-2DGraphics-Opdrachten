91e57326d3eb4712349edb95121a948d29903fc3
Stef Kraaijeveld <itiredplease@gmail.com>
2023-02-08 14:24:15 +0100

    Update Moon.java

--- Week2/001.Moon/src/Moon.java
+++ Week2/001.Moon/src/Moon.java
@@ -34,13 +34,28 @@ public class Moon extends Application {
         graphics.setBackground(Color.white);
         graphics.clearRect(0, 0, (int) canvas.getWidth(), (int) canvas.getHeight());
         graphics.translate(this.canvas.getWidth() / 2, this.canvas.getHeight() / 2);
-        
-        Area moon = new Area(new Ellipse2D.Double(-100, -100, 200, 200));
-        Area cutout = new Area(new Ellipse2D.Double(-100 - 100, -100, 200, 200));
-        Area result = new Area(moon);
-        result.subtract(cutout);
+        graphics.scale(4, -4);
 
-        graphics.fill(result);
+//        Area moon = new Area(new Ellipse2D.Double(-100, -100, 200, 200));
+//        Area cutout = new Area(new Ellipse2D.Double(-100 - 100, -100, 200, 200));
+//        Area result = new Area(moon);
+//        result.subtract(cutout);
+//
+//        graphics.fill(result);
+
+        GeneralPath myShape = new GeneralPath();
+
+        // first line
+        myShape.moveTo(0, 10);
+        myShape.quadTo(5, 15, 0, 20);
+
+        // second line
+        myShape.moveTo(0, 10);
+        myShape.quadTo(12, 15, 0, 20);
+
+
+
+        graphics.draw(myShape);
     }
 
 
