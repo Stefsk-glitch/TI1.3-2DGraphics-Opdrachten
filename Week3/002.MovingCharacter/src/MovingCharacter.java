839780348fb01a34c997c5e200d2f520abd7db35
Stef Kraaijeveld <itiredplease@gmail.com>
2023-02-14 14:10:10 +0100

    moving char

--- Week3/002.MovingCharacter/src/MovingCharacter.java
+++ Week3/002.MovingCharacter/src/MovingCharacter.java
@@ -1,14 +1,13 @@
 import java.awt.*;
 import java.awt.geom.*;
 import java.awt.image.BufferedImage;
-import java.io.IOException;
+import java.io.File;
+import java.io.FileInputStream;
 
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
+import javafx.scene.input.MouseEvent;
 
-import static javafx.application.Application.launch;
-
-import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
@@ -20,46 +19,145 @@ import org.jfree.fx.ResizableCanvas;
 
 public class MovingCharacter extends Application {
     private ResizableCanvas canvas;
+    private int x = 0;
+    private int y = 0;
+    private AffineTransform spriteChar;
+    private BufferedImage image;
+    private BufferedImage[] subImages;
+    private boolean isFalling = false;
+    private boolean isGoingBackwards = false;
+    private int spriteIndex = 42;
+    private int startSpriteIndex = 42;
+    private int endSpriteIndex = 50;
+    private double timer = 0;
 
     @Override
     public void start(Stage stage) throws Exception
     {
+        try
+        {
+            File file = new File("C:\\Users\\Stef\\Desktop\\Git repos\\TI1.3-2DGraphics-Opdrachten\\Week3\\002.MovingCharacter\\resources\\images\\sprite.png");
+            FileInputStream fis = new FileInputStream(file);
+            image = ImageIO.read(fis); //reading the image file
+
+            //cut the image in 65 pieces of 64x64 pixels.
+            subImages = new BufferedImage[65];
+            for (int i = 0; i < 65; i++)
+            {
+                subImages[i] = image.getSubimage(64 * (i % 8), 64 * (i / 8), 64, 64);
+            }
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
 
         BorderPane mainPane = new BorderPane();
         canvas = new ResizableCanvas(g -> draw(g), mainPane);
         mainPane.setCenter(canvas);
-        FXGraphics2D g2d = new FXGraphics2D(canvas.getGraphicsContext2D());
-        new AnimationTimer() {
-            long last = -1;
+        FXGraphics2D graphics = new FXGraphics2D(canvas.getGraphicsContext2D());
+
+        new AnimationTimer()
+        {
+            long lastTime = -1;
 
             @Override
-            public void handle(long now)
+            public void handle(long timeNow)
             {
-                if (last == -1)
-                    last = now;
-                update((now - last) / 1000000000.0);
-                last = now;
-                draw(g2d);
+                if (lastTime == -1)
+                {
+                    lastTime = timeNow;
+                }
+
+                update((timeNow - lastTime) / 1000000000.0);
+                lastTime = timeNow;
+                draw(graphics);
             }
         }.start();
 
         stage.setScene(new Scene(mainPane));
         stage.setTitle("Moving Character");
         stage.show();
-        draw(g2d);
+        draw(graphics);
     }
 
 
     public void draw(FXGraphics2D graphics)
     {
         graphics.setTransform(new AffineTransform());
-        graphics.setBackground(Color.white);
+        graphics.setBackground(Color.darkGray);
         graphics.clearRect(0, 0, (int) canvas.getWidth(), (int) canvas.getHeight());
+
+        spriteChar = new AffineTransform();
+        spriteChar.translate(x, y);
+
+        canvas.setOnMousePressed(event -> mousePressed(event));
+
+        if (isGoingBackwards)
+        {
+            spriteChar.scale(-1, 1);
+        }
+
+        graphics.drawImage(subImages[spriteIndex], spriteChar, null);
     }
 
 
-    public void update(double deltaTime)
+    public void update(double time)
     {
+        timer += time;
+
+        if (spriteIndex == endSpriteIndex)
+        {
+            spriteIndex = startSpriteIndex;
+        }
+
+        if (x > canvas.getWidth() - subImages[spriteIndex].getWidth())
+        {
+            isGoingBackwards = true;
+        }
+
+        if (x < subImages[spriteIndex].getWidth())
+        {
+            isGoingBackwards = false;
+        }
+
+        if (!isGoingBackwards)
+        {
+            x += 4;
+        }
+        else
+        {
+            x -= 4;
+        }
+
+        if (timer > 0.06)
+        {
+            spriteIndex++;
+            timer = 0;
+        }
+
+        if (isFalling)
+        {
+            if (spriteIndex == 25)
+            {
+                isFalling = false;
+                spriteIndex = 42;
+            }
+            if (!isGoingBackwards)
+            {
+                x -= 4;
+            }
+            else
+            {
+                x += 4;
+            }
+        }
+    }
+
+    private void mousePressed(MouseEvent event)
+    {
+        isFalling = true;
+        spriteIndex = 17;
     }
 
     public static void main(String[] args)
